c951c7611fc8fc1c47577cf5afa82153b6c9b114
Camille Arce <thecamillearce@gmail.com>
2023-04-13 04:21:07 +0000

    finished assignment

--- hash_dig.rb
+++ hash_dig.rb
@@ -1,13 +1,21 @@
 # Write a program that uses the sample_hash variable and prints the value of key "history"
 
 sample_hash = {
-   :class => { 
-      :student => { 
-         :name => "Mike",
-         "marks" => { 
-            "physics" => 70,
-            "history" => 80
-         }
-      }
-   }
+  :class => {
+    :student => {
+      :name => "Mike",
+      "marks" => {
+        "physics" => 70,
+        "history" => 80,
+      },
+    },
+  },
 }
+p sample_hash
+class_hash = sample_hash.fetch(:class)
+p class_hash
+student_hash = class_hash.fetch(:student)
+p student_hash
+marks_hash = student_hash.fetch("marks")
+p marks_hash
+p marks_hash.fetch("history")
--- hash_person.rb
+++ hash_person.rb
@@ -2,3 +2,30 @@
 # Make sure the value of the :age key is an Integer
 
 p "Enter a name, age, and occupation separated by spaces:"
+
+person_array = gets.chomp.split(" ")
+
+p person_array
+
+person_hash = { :name => person_array.at(0), :age => person_array.at(1).to_i, :occupation => person_array.at(2) }
+
+p person_hash
+
+=begin
+
+long hand way to code / also right
+person_hash2 = Hash.new
+
+person_hash2.store(:name, person_array.at(0))
+
+person_hash2.store(:age, person_array.at(1).to_i)
+
+person_hash2.store(:occupation, person_array.at(2))
+
+p person_hash2
+=end
+
+# THE CODE BELOW IS WRONG: my first trial and first though process
+#person_array.each |element|
+#person_hash[:name] =
+# end
